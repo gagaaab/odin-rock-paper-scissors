4930704bdc3b818648224e4c9853bb4f5f33ec47
Gabriel Vega <glvega.cc@gmail.com>
2023-09-28 22:23:33 +0800

    Add playRound function: player selects play, returns message if tie game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
-let choices = ["Rock", "Paper", "Scissors"]
+let choices = ["rock", "paper", "scissors"]
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
 console.log(getComputerChoice())
+
+function playRound(playerSelection,computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection) {
+    return "It's a tie! You both picked " + playerSelection.toLowerCase() + "!"
+  }
+}
+
+let playerSelection = prompt("Rock, paper or scissors?", "")
+let computerSelection = getComputerChoice()
+console.log(playRound(playerSelection,computerSelection))
