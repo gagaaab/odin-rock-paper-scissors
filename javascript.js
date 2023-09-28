b7ebff3b7d3ba13b779f6261a84a974dbd5a6d9d
Gabriel Vega <glvega.cc@gmail.com>
2023-09-28 22:49:03 +0800

    Edit playRound function: returns message if win or lose

--- javascript.js
+++ javascript.js
@@ -3,14 +3,36 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-console.log(getComputerChoice())
-
 function playRound(playerSelection,computerSelection) {
-  if (playerSelection.toLowerCase() === computerSelection) {
-    return "It's a tie! You both picked " + playerSelection.toLowerCase() + "!"
+  if (playerSelection === computerSelection) {
+    return "It's a tie! You both picked " + playerSelection + "!"
+  }
+  else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+        return "You lose! Paper beats Rock!"
+    }
+    else {
+        return "You win! Rock beats Scissors!"
+    }
+  }
+  else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+        return "You lose! Scissors beats Paper!"
+    }
+    else {
+        return "You win! Paper beats Rock!"
+    }
+  }
+  else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+        return "You lose! Rock beats Scissors!"
+    }
+    else {
+        return "You win! Scissors beats Paper!"
+    }
   }
 }
 
-let playerSelection = prompt("Rock, paper or scissors?", "")
+let playerSelection = prompt("Rock, paper or scissors?", "").toLowerCase()
 let computerSelection = getComputerChoice()
 console.log(playRound(playerSelection,computerSelection))
