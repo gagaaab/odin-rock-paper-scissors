b1a426bd7f90443bb8467230d2ac32a0ea26c681
Gabriel Vega <glvega.cc@gmail.com>
2023-11-06 19:25:27 +0800

    Add play again button if somebody wins

--- javascript.js
+++ javascript.js
@@ -7,51 +7,48 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection,computerSelection) {
-  if (playerSelection === computerSelection) {
-    alert("It's a tie! You both picked " + playerSelection + "!")
+    if (playerSelection === computerSelection) {
+    message.textContent = "It's a tie! You both picked " + playerSelection + "!"
   }
   else if (playerSelection === "rock") {
     if (computerSelection === "paper") {
         computerScore++;
-        alert("You lose! Paper beats Rock!")
+        message.textContent = "You lose! Paper beats Rock!"
     }
     else {
         humanScore++;
-        alert("You win! Rock beats Scissors!")
+        message.textContent = "You win! Rock beats Scissors!"
     }
   }
   else if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
         computerScore++;
-        alert("You lose! Scissors beats Paper!") 
+        message.textContent = "You lose! Scissors beats Paper!"
     }
     else {
         humanScore++;
-        alert("You win! Paper beats Rock!") 
+        message.textContent = "You win! Paper beats Rock!"
     }
   }
   else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
         computerScore++;
-        alert("You lose! Rock beats Scissors!") 
+        message.textContent = "You lose! Rock beats Scissors!"
     }
     else {
         humanScore++;
-        alert("You win! Scissors beats Paper!") 
+        message.textContent = "You win! Scissors beats Paper!"
     }
   }
 }
-
+const pScore = document.querySelector('#pScore')
+const cScore = document.querySelector('#cScore')
 const rock = document.querySelector('#rock');
     rock.addEventListener('click', () => {
         let playerSelection = 'rock'
         let computerSelection = getComputerChoice()
         playRound(playerSelection,computerSelection)
-        
-        const pScore = document.querySelector('#pScore')
         pScore.textContent = 'Player Score: ' + humanScore;
-        
-        const cScore = document.querySelector('#cScore')
         cScore.textContent = 'Computer Score: ' + computerScore;
         game()});
 
@@ -60,11 +57,7 @@ const paper = document.querySelector('#paper');
         let playerSelection = 'paper'
         let computerSelection = getComputerChoice()
         playRound(playerSelection,computerSelection)
-
-        const pScore = document.querySelector('#pScore')
         pScore.textContent = 'Player Score: ' + humanScore;
-        
-        const cScore = document.querySelector('#cScore')
         cScore.textContent = 'Computer Score: ' + computerScore;
         game()});
 
@@ -73,29 +66,32 @@ const scissors = document.querySelector('#scissors');
         let playerSelection = 'scissors'
         let computerSelection = getComputerChoice()
         playRound(playerSelection,computerSelection)
-        
-        const pScore = document.querySelector('#pScore')
         pScore.textContent = 'Player Score: ' + humanScore;
-        
-        const cScore = document.querySelector('#cScore')
         cScore.textContent = 'Computer Score: ' + computerScore;
         game()});
 
+const message = document.querySelector('#message')
+
 function game() {
     if (humanScore === 5) {
-        alert("You win!");
-        humanScore = 0;
-        computerScore = 0;
-
-        pScore.textContent = 'Player Score: ' + humanScore;        
-        cScore.textContent = 'Computer Score: ' + computerScore;
+        message.textContent = "You win!";
+        button = document.createElement('button')
+        button.id = 'playAgainButton'
+        button.textContent = "Play again"
+        playAgain.appendChild(button)
+        button.addEventListener('click', () => {
+            window.location.reload()
+        })
     }
-    if (computerScore === 5) {
-        alert("You lose!");
-        humanScore = 0;
-        computerScore = 0;
 
-        pScore.textContent = 'Player Score: ' + humanScore;        
-        cScore.textContent = 'Computer Score: ' + computerScore;
+    if (computerScore === 5) {
+        message.textContent = "You lose!";
+        button = document.createElement('button')
+        button.id = 'playAgainButton'
+        button.textContent = "Play again"
+        playAgain.appendChild(button)
+        button.addEventListener('click', () => {
+            window.location.reload()
+        })
     }
 }
